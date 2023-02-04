1bf4f48a870d853b11f9b824ca4fc0d5fe513692
IceBucle <eloiespel@gmail.com>
2023-02-04 16:53:48 +0100

    first commit

---
+++ src/lib/Character.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let character;
+</script>
+
+<div class="character">
+    <img src={character.image} alt={character.status} />
+    <h1>{character.name}</h1>
+    <h3>{character.species} · {character.gender}</h3>
+</div>
